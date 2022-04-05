d2ec047a67b25f5d5123b8a66ee5b78e736c11fe
cyrusn <cyrusncy@gmail.com>
2022-04-05 21:04:38 +0800

    add GitHub Actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? "/react-game-of-15/" : "",
 };
 
 module.exports = nextConfig;
